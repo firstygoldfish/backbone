a3d5788fb4b7479aaf95c4ac1203c315589b22e2
Carl <carl.last@gmail.com>
2023-07-04 10:32:36 +0100

    tut1 updated

--- tut1/client/app.js
+++ tut1/client/app.js
@@ -1,24 +1,10 @@
-websiteModel = Backbone.Model.extend({
-	defaults: {
-		name: 'Backbone app tut1',
-		barcode: 98765
-	},
-	initialize: function() {
-		console.log('Model websiteModel init');
-		this.bindevents();
-	},
-	bindevents: function() {
-		this.on('change:barcode', function(model){
-			console.log('Barcode changed ' + model.get('barcode'));
-		});		
-	}
+
+var officersModel = Backbone.Model.extend({
+    urlRoot: 'localhost:3003/officers'
 });
 
 
-
 $(document).ready(function(){
-	var page1 = new websiteModel();
-	console.log('Page name = ' + page1.get('name') + ' barcode = ' + page1.get('barcode'));
-	page1.set({barcode: 45678});
-	page1.set({barcode: 98765});
+	var anOfficer = new officersModel({id: 'abcd1234'});
+	anOfficer.fetch();
 });
